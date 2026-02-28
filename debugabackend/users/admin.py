eb0eeee00137f118e5f169f02fa2502cec7c51da
yasmine-azzaoui <yasmine-azzaoui>
2026-02-28 11:43:43 +1000

    Fixed some things :)

--- debugabackend/users/admin.py
+++ debugabackend/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username","email")
+    search_fields = ("username", "email")
+    list_filter = ("date_joined",)
+
