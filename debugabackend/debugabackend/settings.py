aac640487007e57e5eadfeb698c2cbd1696474e1
Arrianne <arrianneoshea@gmail.com>
2026-03-02 14:16:16 +1000

    updating the CORS allowed origins to match the frontend local host

--- debugabackend/debugabackend/settings.py
+++ debugabackend/debugabackend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://127.0.0.1:5174',
+    'http://localhost:5174',
 ]
 
 # Application definition
