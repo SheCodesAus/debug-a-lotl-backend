04c448111f287bc9ba679d42d23bfc237d390043
mariabodriv <mariabodero94@gmail.com>
2026-03-13 06:30:06 +1000

    updating book club back end

--- debugabackend/bookclub/urls.py
+++ debugabackend/bookclub/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("clubs/", views.ClubListCreate.as_view()),
+    path("clubs/<int:pk>/", views.ClubDetail.as_view()),
+    path("clubs/<int:pk>/join/", views.ClubJoinView.as_view()),
+    path("clubs/<int:pk>/members/", views.ClubMembersView.as_view()),
+    path(
+        "clubs/<int:club_pk>/members/<int:member_pk>/",
+        views.MemberStatusUpdateView.as_view(),
+    ),
 ]
